cebdd1407e51e3a849efb00403a88e896e334402
Unicorn <tanamet.tpk@gmail.com>
2019-05-12 08:47:11 +0700

    + create project

---
+++ CameraAndVideoEditor/CameraAndVideoEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraAndVideoEditor
+//
+//  Created by Tanamet Tanasinpatcharakul on 12/5/2562 BE.
+//  Copyright © 2562 Tanamet Tanasinpatcharakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
